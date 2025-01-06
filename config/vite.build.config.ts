ffefa7971577167eb75f2eb936bc277fe8efc911
GMELUM <gmelum@vk.com>
2025-01-07 00:24:44 +0300

    initial commit

--- config/vite.build.config.ts
+++ config/vite.build.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+	base: "./",
 	server: {
 		port: 18300,
 		host: '0.0.0.0'
